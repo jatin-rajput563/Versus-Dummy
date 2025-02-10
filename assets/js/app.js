f1d8948d2415addba22c2d896cd0185a49840d76
jatin-rajput563 <jkt3298@gmail.com>
2025-02-11 00:03:31 +0530

    solving-section-mistakes

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,4 +5,39 @@ const navbarbtn = () => {
     document.getElementById("rotats2").classList.toggle("rotate-45")
     document.body.classList.toggle("overflow-hidden")
 }
-$('.carousel').carousel()
+$(".game-slider").slick({
+    dots: false,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: false,
+            },
+        },
+        {
+            breakpoint: 800,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+                centerMode: true,
+                variableWidth: true,
+            },
+        },
+        {
+            breakpoint: 576,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                centerMode: true,
+                variableWidth: true,
+            },
+        },
+    ],
+});
